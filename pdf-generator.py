9ba57c6de42c62258fce2efc42835d2ba36fa7a1
Benjamin Fox <flamekebab@gmail.com>
2021-12-15 18:20:42 +0000

    cleaned up and added docstrings

--- pdf-generator.py
+++ pdf-generator.py
@@ -1,4 +1,4 @@
-# We"ll be using ReportLab to generate PDFs as that"s currently what I"ve got
+# We'll be using ReportLab to generate PDFs as that's currently what I've got
 # If something else is a better fit then I definitely want to hear about it!
 from reportlab.lib.pagesizes import LETTER
 from reportlab.pdfgen.canvas import Canvas
@@ -11,50 +11,52 @@ from reportlab.platypus import Frame, Image
 
 import os
 
-def main(american_user = False): 
-    # We default to A4 but American users may want US Letter instead
-    # I might cut this functionality!
-    if american_user is True:
-        canvas = Canvas("test.pdf", pagesize=LETTER)
-        width, height = LETTER
-    else:
-        canvas = canvas = Canvas("test.pdf")
 
-    # We"re going to need two fonts for these documents
+def main(output_filename: str ="test.pdf"):
+    ''' (TODO: description
+
+    :param output_filename: The path the finished PDF will be written to.
+
+    '''
+
+    canvas = Canvas(output_filename)
+
     # Ridgeline for the titles
-    pdfmetrics.registerFont(TTFont("Ridgeline", "./fonts/Ridgeline.ttf"))
-
     # Source Sans Pro for the body text
-    pdfmetrics.registerFont(TTFont("SSP", "./fonts/SourceSansPro-Regular.ttf"))
-    pdfmetrics.registerFont(TTFont("SSP Italic", "./fonts/SourceSansPro-It.ttf"))
-    pdfmetrics.registerFont(TTFont("SSP Bold", "./fonts/SourceSansPro-Bold.ttf"))
-    pdfmetrics.registerFont(TTFont("SSP Bold Italic", "./fonts/SourceSansPro-BoldIt.ttf"))
+    fonts = (
+        ("Ridgeline", "./fonts/Ridgeline.ttf"),
+        ("SSP", "./fonts/SourceSansPro-Regular.ttf"),
+        ("SSP Italic", "./fonts/SourceSansPro-It.ttf"),
+        ("SSP Bold", "./fonts/SourceSansPro-Bold.ttf"),
+        ("SSP Bold Italic", "./fonts/SourceSansPro-BoldIt.ttf")
+    )
 
+    for font in fonts:
+        add_font(font[0], font[1])
+
+    # addMapping amends SSP to have multiple styles - rather than switching font when the style changes.
     # addMapping(face, bold, italic, psname)
-    addMapping('SSP', 0, 0, 'SSP') #normal
-    addMapping('SSP', 0, 1, 'SSP Italic') #italic
-    addMapping('SSP', 1, 0, 'SSP Bold') #bold
-    addMapping('SSP', 1, 1, 'SSP Bold Italic') #italic and bold
+    addMapping('SSP', 0, 0, 'SSP')  # normal
+    addMapping('SSP', 0, 1, 'SSP Italic')  # italic
+    addMapping('SSP', 1, 0, 'SSP Bold')  # bold
+    addMapping('SSP', 1, 1, 'SSP Bold Italic')  # italic and bold
 
     # From now on we can use the syntax "canvas.setFont("Ridgeline", 32)" to set the font and size
-
-    # We're going to need to draw a box for this image but that can happen later
-    
-    footer = "./footers/bottom-bar-R.png"
-    # the 0s are anchoring co-ordinates. The anchor parameter is a compass reference 
-    # "ne", "sw" with "c" being the centre of the compass
-    # 19.5cm is the correct value - just trust my maths on this ;)
-    canvas.drawImage(footer, 0, 0, width=19.5*cm, preserveAspectRatio=True, anchor='c')
-
-
-
-
     canvas.setFont("Ridgeline", 18)
     canvas.drawString(72, 72, "Hello, World")
-    
+
     canvas.save()
 
 
+def add_font(name: str, path: str):
+    ''' Non-default fonts must be registered before use in PDF generation.
+
+    :param name: The name you want the font registered as (can be different from the font's original name)
+
+    :param path: Either an absolute path to the font's TTF file or a path relative to where this function is run.
+
+    '''
+    pdfmetrics.registerFont(TTFont(name, path))
 
 
 if __name__ == "__main__":
