de984f207c027fd750cf766875682c51a32f4510
Michael Haslam <hreikin@gmail.com>
2021-12-16 15:44:30 +0000

    Add some basic comments to explain the code.

--- extract_metadata.py
+++ extract_metadata.py
@@ -1,10 +1,15 @@
 from PyPDF2 import PdfFileReader
 def get_metadata(input_file):
+
+    # Opens (and closes because we are using "with".) provided input file in read mode.
     with open(input_file, 'rb') as metadata_file:
+
+        # Creates the file reader and gets the metadata and number of pages
         pdf_file_reader = PdfFileReader(metadata_file)
         metadata = pdf_file_reader.getDocumentInfo()
         number_of_pages = pdf_file_reader.getNumPages()
 
+    # Prints out the metadata and number of pages.
     for k, v in metadata.items():
         print(k +":", v)
     print(f"Number of pages: {number_of_pages}")
