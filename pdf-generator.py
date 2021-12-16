b71f98f2f624f8f2b89f5f46e5668a371621e524
Benjamin Fox <flamekebab@gmail.com>
2021-12-16 18:20:40 +0000

    Various bits of cleanup

--- pdf-generator.py
+++ pdf-generator.py
@@ -1,24 +1,21 @@
 # We'll be using ReportLab to generate PDFs as that's currently what I've got
 # If something else is a better fit then I definitely want to hear about it!
-from reportlab.lib.pagesizes import LETTER
-from reportlab.pdfgen.canvas import Canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.lib.fonts import addMapping
-from reportlab.lib import utils
 from reportlab.lib.units import mm
-from reportlab.platypus import Frame, Image
 from reportlab.platypus import BaseDocTemplate, Frame, Paragraph, PageBreak, PageTemplate
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-import os
 
 
-def main(output_filename: str ="test.pdf"):
-    ''' (TODO: description
+def main(body_text_input: str, output_filename: str = "test.pdf"):
+    """ Generates PDFs with arbitrary text content for merging with existing blank PDFs.
+
+    :param body_text_input: Currently any old string - to be modified later
 
     :param output_filename: The path the finished PDF will be written to.
 
-    '''
+    """
 
     # Ridgeline for the titles
     # Source Sans Pro for the body text
@@ -41,9 +38,15 @@ def main(output_filename: str ="test.pdf"):
     addMapping('SSP', 1, 1, 'SSP Bold Italic')  # italic and bold
 
     # Add the style elements
-    add_tUGS_Style()
+    add_tugs_style()
     # These dimensions worked out from existing PDFs
-    doc = BaseDocTemplate('test.pdf', showBoundary=1, leftMargin=14.5 * mm, rightMargin=14.5 * mm, bottomMargin=35 * mm, topMargin=30 * mm)
+    doc = BaseDocTemplate(
+        output_filename,
+        showBoundary=1,
+        leftMargin=14.5 * mm,
+        rightMargin=14.5 * mm,
+        bottomMargin=35 * mm,
+        topMargin=30 * mm)
 
     # As yet undecided on whether to allow header paragraphs (all bold SSP, centre aligned)
     # If so push the paragraphs down.
@@ -59,45 +62,37 @@ def main(output_filename: str ="test.pdf"):
     paragraphs = []
 
     # <br/> tags work but <br> causes errors
-    bacon_ipsum = """Pork belly ut enim aliquip andouille irure. Ground round velit brisket shoulder, eiusmod tri-tip dolor. Minim rump beef, tenderloin voluptate do capicola labore landjaeger ea quis bacon et. Pork chop tempor shankle hamburger nulla.
-
-    Cow ut doner ipsum fugiat aliquip. Proident pork loin minim nostrud bacon, beef ball tip ullamco. Short loin porchetta pig, dolore nulla ex ut ham hock kielbasa bresaola swine ipsum excepteur tongue veniam. Dolor doner ball tip, tail tenderloin capicola nostrud bacon. Quis shankle t-bone kevin, anim officia sunt excepteur corned beef short ribs spare ribs laboris in voluptate. Pancetta sunt pork chop burgdoggen tenderloin frankfurter. Brisket fugiat adipisicing filet mignon.
-
-    Velit spare ribs alcatra, excepteur in filet mignon ground round nostrud frankfurter drumstick tail.Leberkas in in brisket venison ribeye nostrud sunt quis spare ribs ullamco nisi adipisicing boudin pig. Cow meatloaf eu flank, pancetta magna commodo enim strip steak in. Chuck quis spare ribs turducken, capicola beef brisket salami doner.
-
-    Aliqua tri-tip shankle ribeye hamburger jerky filet mignon pork chop turkey. Aliquip flank mollit eiusmod. Veniam tempor reprehenderit laboris. Quis jerky dolor, picanha esse irure tempor ut laboris biltong. Qui labore tail minim cupidatat turkey aute eu anim porchetta. Biltong ball tip porchetta, non cupim t-bone deserunt consectetur ad irure pig shankle tri-tip frankfurter beef ribs.
-
-    Swine pork belly rump, nostrud ham hock cow boudin. Adipisicing dolore capicola in dolor hamburger. Cupidatat reprehenderit drumstick chislic tri-tip short loin aliqua buffalo tail burgdoggen pork fugiat porchetta. Nostrud eiusmod proident pork chop. Andouille alcatra dolor cow dolore porchetta."""
 
     paragraphs.append(Paragraph("Heading 1", styles['document_title']))
-    paragraphs.append(Paragraph(bacon_ipsum, styles['body_text']))
+    paragraphs.append(Paragraph(body_text_input, styles['body_text']))
     paragraphs.append(Paragraph("Heading 2", styles['subheading']))
-    paragraphs.append(Paragraph(bacon_ipsum, styles['body_text']))
+    paragraphs.append(Paragraph(body_text_input, styles['body_text']))
     doc.addPageTemplates([PageTemplate(id='TwoCol', frames=[frame1, frame2]), ])
 
     doc.build(paragraphs)
 
 
 def add_font(name: str, path: str):
-    ''' Non-default fonts must be registered before use in PDF generation.
+    """ Non-default fonts must be registered before use in PDF generation.
 
     :param name: The name you want the font registered as (can be different from the font's original name)
 
     :param path: Either an absolute path to the font's TTF file or a path relative to where this function is run.
 
-    '''
+    """
     pdfmetrics.registerFont(TTFont(name, path))
 
-def add_tUGS_Style():
-    ''' Abstracting this out to make changes to the style easier.
 
-    '''
+def add_tugs_style():
+    """ Abstracting this out to make changes to the style easier.
+
+    """
 
     # The sample stylesheet provides a base
     global styles
     styles = getSampleStyleSheet()
     # To see the various existing style components use:
-    #print(styles.list())
+    # print(styles.list())
 
     # Needed things - H1, H2, body text
     h1 = ParagraphStyle(
@@ -121,7 +116,7 @@ def add_tUGS_Style():
         parent=styles['BodyText'],
         fontName="SSP",
         fontSize=14,
-        alignment=4, # page 77 of the Reference PDF (4 = justify)
+        alignment=4,  # page 77 of the Reference PDF (4 = justify)
         spaceAfter=14,
         bulletFontName="SSP"
         )
@@ -129,5 +124,16 @@ def add_tUGS_Style():
     styles.add(h2)
     styles.add(body_paragraph)
 
+
 if __name__ == "__main__":
-    main()
+    bacon_ipsum = """Pork belly ut enim aliquip andouille irure. Ground round velit brisket shoulder, eiusmod tri-tip dolor. Minim rump beef, tenderloin voluptate do capicola labore landjaeger ea quis bacon et. Pork chop tempor shankle hamburger nulla.
+
+        Cow ut doner ipsum fugiat aliquip. Proident pork loin minim nostrud bacon, beef ball tip ullamco. Short loin porchetta pig, dolore nulla ex ut ham hock kielbasa bresaola swine ipsum excepteur tongue veniam. Dolor doner ball tip, tail tenderloin capicola nostrud bacon. Quis shankle t-bone kevin, anim officia sunt excepteur corned beef short ribs spare ribs laboris in voluptate. Pancetta sunt pork chop burgdoggen tenderloin frankfurter. Brisket fugiat adipisicing filet mignon.
+
+        Velit spare ribs alcatra, excepteur in filet mignon ground round nostrud frankfurter drumstick tail.Leberkas in in brisket venison ribeye nostrud sunt quis spare ribs ullamco nisi adipisicing boudin pig. Cow meatloaf eu flank, pancetta magna commodo enim strip steak in. Chuck quis spare ribs turducken, capicola beef brisket salami doner.
+
+        Aliqua tri-tip shankle ribeye hamburger jerky filet mignon pork chop turkey. Aliquip flank mollit eiusmod. Veniam tempor reprehenderit laboris. Quis jerky dolor, picanha esse irure tempor ut laboris biltong. Qui labore tail minim cupidatat turkey aute eu anim porchetta. Biltong ball tip porchetta, non cupim t-bone deserunt consectetur ad irure pig shankle tri-tip frankfurter beef ribs.
+
+        Swine pork belly rump, nostrud ham hock cow boudin. Adipisicing dolore capicola in dolor hamburger. Cupidatat reprehenderit drumstick chislic tri-tip short loin aliqua buffalo tail burgdoggen pork fugiat porchetta. Nostrud eiusmod proident pork chop. Andouille alcatra dolor cow dolore porchetta."""
+
+    main(bacon_ipsum)
