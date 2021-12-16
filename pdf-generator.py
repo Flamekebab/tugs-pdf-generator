6acdb2e850f434347b74ba44acddf3c1d29432fc
Benjamin Fox <flamekebab@gmail.com>
2021-12-16 02:47:09 +0000

    Demystifying ReportLab. A bit.

--- pdf-generator.py
+++ pdf-generator.py
@@ -8,7 +8,8 @@ from reportlab.lib.fonts import addMapping
 from reportlab.lib import utils
 from reportlab.lib.units import cm
 from reportlab.platypus import Frame, Image
-
+from reportlab.platypus import BaseDocTemplate, Frame, Paragraph, PageBreak, PageTemplate
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 import os
 
 
@@ -43,9 +44,70 @@ def main(output_filename: str ="test.pdf"):
 
     # From now on we can use the syntax "canvas.setFont("Ridgeline", 32)" to set the font and size
     canvas.setFont("Ridgeline", 18)
-    canvas.drawString(72, 72, "Hello, World")
+    #canvas.drawString(72, 72, "Hello, World")
 
-    canvas.save()
+
+    # Building a stylesheet from scratch is a hassle, instead we use the sample stylsheet as a base
+    styles = getSampleStyleSheet()
+    # To see the various options use
+    #print(styles.list())
+
+    # Needed things - H1, H2, body text
+    h1 = ParagraphStyle(
+        'document_title',
+        parent=styles['Heading1'],
+        fontName="Ridgeline",
+        fontSize=50,
+        alignment=1,
+        spaceAfter=50,
+        )
+    h2 = ParagraphStyle(
+        'subheading',
+        parent=styles['Heading3'],
+        fontName="Ridgeline",
+        fontSize=20,
+        alignment=0,
+        spaceAfter=20,
+        )
+    body_paragraph = ParagraphStyle(
+        'body_text',
+        parent=styles['BodyText'],
+        fontName="SSP",
+        fontSize=14,
+        alignment=4, # page 77 of the Reference PDF (4 = justify)
+        spaceAfter=14,
+        bulletFontName="SSP"
+        )
+    styles.add(h1)
+    styles.add(h2)
+    styles.add(body_paragraph)
+
+
+    doc = BaseDocTemplate('test.pdf', showBoundary=1, leftMargin=0.553, rightMargin=0.553)
+
+    frame1 = Frame(doc.leftMargin, doc.bottomMargin, doc.width / 2 - 6, doc.height, id='col1')
+    frame2 = Frame(doc.leftMargin + doc.width / 2 + 6, doc.bottomMargin, doc.width / 2 - 6, doc.height, id='col2')
+    paragraphs = []
+
+    bacon_ipsum = """Pork belly ut enim aliquip andouille irure. Ground round velit brisket shoulder, eiusmod tri-tip dolor. Minim rump beef, tenderloin voluptate do capicola labore landjaeger ea quis bacon et. Pork chop tempor shankle hamburger nulla.
+
+    Cow ut doner ipsum fugiat aliquip. Proident pork loin minim nostrud bacon, beef ball tip ullamco. Short loin porchetta pig, dolore nulla ex ut ham hock kielbasa bresaola swine ipsum excepteur tongue veniam. Dolor doner ball tip, tail tenderloin capicola nostrud bacon. Quis shankle t-bone kevin, anim officia sunt excepteur corned beef short ribs spare ribs laboris in voluptate. Pancetta sunt pork chop burgdoggen tenderloin frankfurter. Brisket fugiat adipisicing filet mignon.
+
+    Velit spare ribs alcatra, excepteur in filet mignon ground round nostrud frankfurter drumstick tail. Leberkas in in brisket venison ribeye nostrud sunt quis spare ribs ullamco nisi adipisicing boudin pig. Cow meatloaf eu flank, pancetta magna commodo enim strip steak in. Chuck quis spare ribs turducken, capicola beef brisket salami doner.
+
+    Aliqua tri-tip shankle ribeye hamburger jerky filet mignon pork chop turkey. Aliquip flank mollit eiusmod. Veniam tempor reprehenderit laboris. Quis jerky dolor, picanha esse irure tempor ut laboris biltong. Qui labore tail minim cupidatat turkey aute eu anim porchetta. Biltong ball tip porchetta, non cupim t-bone deserunt consectetur ad irure pig shankle tri-tip frankfurter beef ribs.
+
+    Swine pork belly rump, nostrud ham hock cow boudin. Adipisicing dolore capicola in dolor hamburger. Cupidatat reprehenderit drumstick chislic tri-tip short loin aliqua buffalo tail burgdoggen pork fugiat porchetta. Nostrud eiusmod proident pork chop. Andouille alcatra dolor cow dolore porchetta."""
+
+    paragraphs.append(Paragraph("Heading 1", styles['document_title']))
+    paragraphs.append(Paragraph(bacon_ipsum, styles['body_text']))
+    paragraphs.append(Paragraph("Heading 2", styles['subheading']))
+    paragraphs.append(Paragraph(bacon_ipsum, styles['body_text']))
+    doc.addPageTemplates([PageTemplate(id='TwoCol', frames=[frame1, frame2]), ])
+
+    doc.build(paragraphs)
+
+    #canvas.save()
 
 
 def add_font(name: str, path: str):
